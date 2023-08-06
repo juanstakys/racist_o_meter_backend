4fb4cdbdb6ebbc9530434083c0b769faa875d118
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-08-05 23:16:20 -0300

    Request handling

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ app.use(express.json());
 app.post('/deteccion', (req, res) => {
 
     const { statement } = req.body
-    var esRacista = calculateRacism(statement);
 
     if (!statement) {
-        res.status(401).send({message:'missing parameter statement in body request.'});
+        res.status(400).send({message:'missing parameter statement in body request.'});
+    } else {
+        var esRacista = calculateRacism(statement);
+        res.send({
+            receivedStatement : statement,
+            isItRacist: esRacista,
+            explanation: 'This is an explanation'
+        })
     }
-    res.send({
-        receivedStatement : statement,
-        isItRacist: esRacista,
-        explanation: 'This is an explanation'
-    })
 });
 
 app.listen(PORT, '0.0.0.0',() => {
