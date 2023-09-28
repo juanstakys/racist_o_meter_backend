23da48ac65a6e1470f223198066ede9ff9a4f547
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-09-27 22:11:56 -0300

    Database connection

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const { getAIResponse } = require('./gpt_functions.js');
+const { MongoClient } = require('mongodb');
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = 8080;
 
+// Mongo database connection
+const mongo_uri = require('./atlas_uri.js');
+const client = new MongoClient(mongo_uri);
+const dbname = "racist-o-meter"
+
+const connectToDatabase = async () => {
+    try{
+        await client.connect();
+        console.log(`Successfully connected to the ${dbname} database`);
+    } catch (err) {
+        console.error(`Error connecting to the database: ${err}`);
+    }
+}
+
+const main = async () => {
+    try {
+        await connectToDatabase();
+        
+    } catch (err) {
+        console.error(`Error connecting to the database: ${err}`);
+    } finally {
+        await client.close();
+    }
+}
+
+main()
+
+// Express
 app.use(cors());
 app.use(express.json());
 
